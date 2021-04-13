d9e6017f6cdef432260dca9a44c4ce070a87f035
MJ Galam <mjgalam0907@gmail.com>
2021-04-13 15:52:38 -0700

    Adds an array with three objects and adds prompting for user to do.

---
+++ JS/app.js
@@ -0,0 +1,25 @@
+const courseArray = [
+    {
+        code: "ACIT 1620", 
+        name: "Fundamental Web Technologies", 
+        term: "Winter 2021",
+    },
+
+    {
+        code: "ACIT 1420",
+        name: "Intro to Sys. Admin",
+        term: "Fall 2020",
+    },
+
+    {
+        code: "ORGB 1100",
+        name: "Organizational Behaviour",
+        term: "Winter 2021",
+    },
+];
+
+let courseCode;
+do{
+    courseCode = prompt("Enter code: ");
+} while(isNaN(courseCode) === true && courseCode.length > 3)
+
